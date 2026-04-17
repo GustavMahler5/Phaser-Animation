916f3bd839e3c09b7fbb9c3d74091c6bed8abe4c
Godot107 <Jasonholtman15@gmail.com>
2026-04-16 17:35:26 -0700

    Created webpage, installed phaser

---
+++ game.js
@@ -0,0 +1,21 @@
+console.log("game.js is running")
+
+class myGame extends Phaser.Scene {
+    constructor() {
+        super("myGame")
+    }
+
+    preload() {
+    }
+
+    build(){
+
+    }
+    create() {
+    }
+    update(){
+
+    }
+}
+
+let game = new Phaser.Game();
