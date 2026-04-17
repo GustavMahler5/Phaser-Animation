eeb67aebb37a3acb30a61f7ede2144554c1db7b4
Jason Holtman <jasonholtman15@gmail.com>
2026-04-17 15:55:46 -0700

    Refactor myGame class to MyGame and update config

--- game.js
+++ game.js
@@ -1,14 +1,27 @@
 console.log("game.js is running")
 
-class myGame extends Phaser.Scene {
+class MyGame extends Phaser.Scene {
     constructor() {
-        super("myGame")
+        super("myGame");
     }
 
     preload() {
+        this.load.setPath("assets/");
+        this.load.image("Character", "CharacterData.png");
+        this.load.image("Background", "Background.png");
+        this.load.audio("Intro", "Intro.mp3");
+        this.load.audio("MainMenu", "MainMenu.mp3");
     }
 
     create() {
+        this.graphics = this.add.graphics();
+        let intro = this.sound.add("Intro", { loop: false });
+        let mainMenu = this.sound.add("MainMenu", { loop: true });
+        let background = this.add.image(400, 300, "Background");
+        let character = this.add.image(400, 300, "Character");
+        let circle = this.graphics.fillCircle(400, 300, 50, 0x00ffff);
+
+        // intro.play();
     }
 
     update() {
@@ -20,7 +33,7 @@ let config = {
     width: 800,
     height: 600,
     backgroundColor: "#000000",
-    scene: myGame
+    scene: MyGame
 }
 
-let game = new Phaser.Game();
+let game = new Phaser.Game(config);
